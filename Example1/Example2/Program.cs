8ec952dd2b721a58213f49142c1537d76db3351c
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-04-08 17:34:20 -0500

    Example 2

---
+++ Example1/Example2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter your grade. >>");
+                answer = Console.ReadLine();
+                string letterGrade = ConvertGradeToLetterGrade(Convert.ToDouble(answer));
+                Console.WriteLine($"Congrats you have a {letterGrade}!");
+
+                //Console.WriteLine($"Congrats you have a {ConvertGradeToLetterGrade(Convert.ToDouble(answer))}!");
+
+                Console.WriteLine("Do you want to convert another grade? Yes or No?");
+                answer = Console.ReadLine();
+                
+            } while (answer.ToLower()[0] == 'y');
+
+
+
+        }
+
+        /// <summary>
+        /// Converts a grade(80.78) to a letter grade (C)
+        /// </summary>
+        /// <param name="grade">A double representing the grade</param>
+        /// <returns>The letter grade associated with the grade, and N/A if the grade is below 0</returns>
+        static string ConvertGradeToLetterGrade(double grade)
+        {
+            string letter = "";
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+            else if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else if (grade >= 0)
+            {
+                letter = "F";
+            }
+            else
+            {
+                letter = "N/A";
+            }
+
+            return letter;
+        }
+    }
+}
